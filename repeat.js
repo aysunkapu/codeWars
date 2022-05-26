0975b906e0b407a146a78a919f277089d66d913f
Aysun Kapusuz <aysunkapusuz@gmail.com>
2022-05-26 14:21:53 -0600

    new solution

---
+++ repeat.js
@@ -0,0 +1,7 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+
+function repeatStr (s,n){
+    return s.repeat(n)
+}
+console.log(repeatStr('Hello', 12))
