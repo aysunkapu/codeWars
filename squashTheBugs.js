0663dea9e53d7a3922afebaf4ed7f1fca16de859
Aysun <aysunkapusuz@gmail.com>
2022-06-02 08:24:35 -0600

    new solution

---
+++ squashTheBugs.js
@@ -0,0 +1,6 @@
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+// 
+// There will only be one 'longest' word.
+
+findLongest = s => s.split(" ").reduce((l, e) => l = l < e.length ? e.length : l, 0);
+
