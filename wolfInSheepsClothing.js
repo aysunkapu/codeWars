7c7976c162dfd945305ab6888874424e5bd3d194
Aysun <aysunkapusuz@gmail.com>
2022-06-07 09:13:31 -0600

    another solution added

--- wolfInSheepsClothing.js
+++ wolfInSheepsClothing.js
@@ -14,3 +14,10 @@
 // 
 // Input: ["sheep", "sheep", "wolf"]
 // Output: "Pls go away and stop eating my sheep"
+
+function warnTheSheep (queue){
+    const position = queue.reverse().indexOf('wolf')
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`
+}
+
+console.log(warnTheSheep(['sheep','wolf','sheep','sheep']))
