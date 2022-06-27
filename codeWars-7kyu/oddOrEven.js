400fbf9a794f26df972d25af7093d92c2861e1d1
Aysun <aysunkapusuz@gmail.com>
2022-06-27 10:20:00 -0600

    practice

---
+++ codeWars-7kyu/oddOrEven.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Task
+// Given a number N, determine if the sum of N consecutive numbers is odd or even.
+// 
+// If the sum is definitely an odd number, return Odd.
+// If the sum is definitely an even number, return Even.
+// If the sum can be either odd or even ( depending on which first number you choose ), return Either.
