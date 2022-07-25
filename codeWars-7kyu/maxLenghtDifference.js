38774d9a3ea78b4ec41c62413500e45c501b34a1
Aysun <aysunkapusuz@gmail.com>
2022-07-25 12:38:57 -0600

    another solution

--- codeWars-7kyu/maxLenghtDifference.js
+++ codeWars-7kyu/maxLenghtDifference.js
@@ -13,9 +13,11 @@
 // input : 2 strings with substrings separated by ,
 // output: number as a string
 
-function maxLength(a1,a2){
-    if(a1.length=== 0 || a2.legth===0) return -1
+function maxLen(a1, a2) {
+    if (a1.length === 0 || a2.length === 0) return -1
     let l1 = a1.map(str => str.length)
     let l2 = a2.map(str => str.length)
     return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
-}
+  }
+
+console.log(maxLen('a', "ncn"))
