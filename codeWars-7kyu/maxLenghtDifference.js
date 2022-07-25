d89fc6db103bf2461fb0b6fb23a5166ddf26f22a
Aysun <aysunkapusuz@gmail.com>
2022-07-25 12:33:01 -0600

    another solution

--- codeWars-7kyu/maxLenghtDifference.js
+++ codeWars-7kyu/maxLenghtDifference.js
@@ -12,3 +12,10 @@
 // Bash note:
 // input : 2 strings with substrings separated by ,
 // output: number as a string
+
+function maxLength(a1,a2){
+    if(a1.length=== 0 || a2.legth===0) return -1
+    let l1 = a1.map(str => str.length)
+    let l2 = a2.map(str => str.length)
+    return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
