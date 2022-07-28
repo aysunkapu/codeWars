76103de0f33f519a6e129f14456ed45b9c4f0205
Aysun <aysunkapusuz@gmail.com>
2022-07-28 11:39:03 -0600

    new solution

--- codeWars-7kyu/findAnonymousFunction.js
+++ codeWars-7kyu/findAnonymousFunction.js
@@ -4,3 +4,7 @@
 // Your input. First Parameter will be an array with an anonymous function somewhere in the lot, The second Parameter will be an array which you will filter using the anonymous function you find.
 // Output
 // Your output. Output a filtered version of the second parameter using the function found in the first parameter.
+
+function findFuct(funct, arr){
+    return arr.filter(funct.filter(f => typeof f == 'function')[0])
+}
