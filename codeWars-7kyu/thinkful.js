82b83f201855c40daa98f15ddcd6a72cc2152a3a
Aysun <aysunkapusuz@gmail.com>
2022-08-02 10:23:41 -0600

    new solution added

--- codeWars-7kyu/thinkful.js
+++ codeWars-7kyu/thinkful.js
@@ -30,10 +30,10 @@
 
 class Quark {
     constructor (color, flavor) {
-        Object.assign(this, {color, flavor, baryon_number: 1 / 3})
+        Object.assign(this, {color, flavor, baryon_number: 1 / 3});
     }
 
     interact (quark){
-        [this.color, quark.color] = [quark.color, this.color]
+        [this.color, quark.color] = [quark.color, this.color];
     }
 }
