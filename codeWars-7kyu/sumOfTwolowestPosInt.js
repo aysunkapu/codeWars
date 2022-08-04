aedcf102e0f1eb8b1c452e5860d4a9b7249a1ac6
Aysun <aysunkapusuz@gmail.com>
2022-08-04 10:03:10 -0600

    another solution added

--- codeWars-7kyu/sumOfTwolowestPosInt.js
+++ codeWars-7kyu/sumOfTwolowestPosInt.js
@@ -4,3 +4,11 @@
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 // 
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumOfTwoLowestIntegers(numbers){
+    let [x,y] = numbers.sort((x,y)=> x - y)
+    return x + y
+
+}
+
+console.log(sumOfTwoLowestIntegers([1,2,3,4,5]))
