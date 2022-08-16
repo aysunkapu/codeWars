1d15d82220662d11ad12d85a46788028c24b3eff
Aysun <aysunkapusuz@gmail.com>
2022-08-16 08:33:19 -0600

    another codewar solution

--- codeWars-7kyu/productOfMax.js
+++ codeWars-7kyu/productOfMax.js
@@ -29,4 +29,4 @@ function maxProduct(numbers, size){
     return numbers.sort((a,b) => b-a).slice(0,size).reduce((acc,n) => acc*n);
   }
 
-  console.log(maxProduct([1,2,3,4], 2))
+  console.log(maxProduct([1,2,3,4], 3))
