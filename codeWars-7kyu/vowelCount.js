d53d93aa42409154c017d87a5bce3af3f28b6903
Aysun <aysunkapusuz@gmail.com>
2022-08-17 16:33:52 -0600

    practice

--- codeWars-7kyu/vowelCount.js
+++ codeWars-7kyu/vowelCount.js
@@ -2,3 +2,5 @@ DESCRIPTION:
 Return the number (count) of vowels in the given string.
 
 We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
