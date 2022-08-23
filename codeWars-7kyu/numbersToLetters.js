d93be42e32374df7292d8ffb4ee4401f8fb15efc
Aysun <aysunkapusuz@gmail.com>
2022-08-23 09:50:46 -0600

    another solution

--- codeWars-7kyu/numbersToLetters.js
+++ codeWars-7kyu/numbersToLetters.js
@@ -4,9 +4,9 @@
 // 
 // All inputs will be valid.
 
-function numsToLetters (num){
+function numsToLetters (x){
     const arr = ['', "z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a", "!", "?"," "];
-    return num.map(Number).map(el => arr[el]).join('')
+    return x.map(Number).map(el => arr[el]).join('')
 }
-console.log(numsToLetters(1))
+console.log(numsToLetters('y'))
 
