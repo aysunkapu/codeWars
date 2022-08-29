a6ee5f2b6c010a4fe1853e32fa0e57e89dea88c2
Aysun <aysunkapusuz@gmail.com>
2022-08-29 09:36:36 -0600

    another solution

--- codeWars-7kyu/findTheBalance.js
+++ codeWars-7kyu/findTheBalance.js
@@ -1,48 +1,48 @@
-Description:
-#5 is too hard? Sorry, I'll try to make kata easier(just try), 5kyu or 6kyu? ;-) Let's begin #6:
-
-Given an array arr with positive integers, and more than 2 elements, return an array with the indexes of those elements, such as the average of its left side equals the average of its right side. The comparison will be between integer values, so you should round the average value first(to the nearest integer), and then do the comparison. If no element fulfills such condition, just return an empty array [].
-
-Examples
-findBalance([1,2,1]) === [1]
-
-The balance point at index 1:
-Left     Middle     Right
- 1         2          1
-left == right
-
-findBalance([1,2,3]) === []
-
-No balance point found, return an empty array.
-
-findBalance([1,2,3,2,1]) === [2]
-
-The balance point at index 1:
- Left     Middle     Right
- 1,2        3         2,1
-left  --> (1 + 2) / 2 --> 1.5 --> round(1.5) --> 2
-right --> (2 + 1) / 2 --> 1.5 --> round(1.5) --> 2
-left == right
-
-findBalance([1,1,1,1,1]) === [1,2,3]
-
-The balance point at index 1,2 and 3:
- Left     Middle     Right
-  1         1        1,1,1
- 1,1        1         1,1
-1,1,1       1          1
-In these 3 situations, the average of left side and 
-right side all are 1.
-
-findBalance([1,1,2,1,1]) === [1,2,3]
-
-The balance point at index 1,2 and 3:
- Left     Middle     Right
-  1         1        2,1,1
- 1,1        2         1,1
-1,1,2       1          1
-In these 3 situations, the average of left side and 
-right side all are 1(after rounding operation).
-
-
-findBalance([1,1,3,1,1]) === [2]
+// Description:
+// #5 is too hard? Sorry, I'll try to make kata easier(just try), 5kyu or 6kyu? ;-) Let's begin #6:
+// 
+// Given an array arr with positive integers, and more than 2 elements, return an array with the indexes of those elements, such as the average of its left side equals the average of its right side. The comparison will be between integer values, so you should round the average value first(to the nearest integer), and then do the comparison. If no element fulfills such condition, just return an empty array [].
+// 
+// Examples
+// findBalance([1,2,1]) === [1]
+// 
+// The balance point at index 1:
+// Left     Middle     Right
+//  1         2          1
+// left == right
+// 
+// findBalance([1,2,3]) === []
+// 
+// No balance point found, return an empty array.
+// 
+// findBalance([1,2,3,2,1]) === [2]
+// 
+// The balance point at index 1:
+//  Left     Middle     Right
+//  1,2        3         2,1
+// left  --> (1 + 2) / 2 --> 1.5 --> round(1.5) --> 2
+// right --> (2 + 1) / 2 --> 1.5 --> round(1.5) --> 2
+// left == right
+// 
+// findBalance([1,1,1,1,1]) === [1,2,3]
+// 
+// The balance point at index 1,2 and 3:
+//  Left     Middle     Right
+//   1         1        1,1,1
+//  1,1        1         1,1
+// 1,1,1       1          1
+// In these 3 situations, the average of left side and 
+// right side all are 1.
+// 
+// findBalance([1,1,2,1,1]) === [1,2,3]
+// 
+// The balance point at index 1,2 and 3:
+//  Left     Middle     Right
+//   1         1        2,1,1
+//  1,1        2         1,1
+// 1,1,2       1          1
+// In these 3 situations, the average of left side and 
+// right side all are 1(after rounding operation).
+// 
+// 
+// findBalance([1,1,3,1,1]) === [2]
