8628c8022edc37a4351f2ff32be05bdc4f17cb7d
Aysun <aysunkapusuz@gmail.com>
2022-09-01 16:28:23 -0600

    another codewar solution

--- codeWars-7kyu/trimmingastring.js
+++ codeWars-7kyu/trimmingastring.js
@@ -12,3 +12,7 @@
 // If the string is smaller or equal than the maximum string length, then simply return the string with no trimming or dots required.
 // 
 // e.g. trim("Code Wars is pretty rad", 50) should return "Code Wars is pretty rad"
+
+function trim(arr, size) {
+    return arr.length <= size ? arr : arr.slice(0, arr.length > 3 ? size - 3 : size) + '...'
+}
