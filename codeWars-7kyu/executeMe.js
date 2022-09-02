e32da37d5fdfebed4dc049eb473baffd7ad3338b
Aysun <aysunkapusuz@gmail.com>
2022-09-02 10:13:09 -0600

    new codewar

---
+++ codeWars-7kyu/executeMe.js
@@ -0,0 +1,2 @@
+// DESCRIPTION:
+// You're given a procedure / method that executes a passed action n times, but it does so in a purely sequential manner. If you inspect the sample tests, you'll find that the action takes about 1 second to complete, and this action is repeated 20 times which results in a timeout.
