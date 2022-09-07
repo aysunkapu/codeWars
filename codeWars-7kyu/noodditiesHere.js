4baf298f7a9da3b89c1a9442a65f66554849b02e
Aysun <aysunkapusuz@gmail.com>
2022-09-07 07:49:21 -0600

    new solution

--- codeWars-7kyu/noodditiesHere.js
+++ codeWars-7kyu/noodditiesHere.js
@@ -1,3 +1,12 @@
 // write a small function that returns the values of an array that are not odd.
 // all values in the arrar will be integers. return the goood values in the order they are given.
 // function noOdds(values)
+
+function noOdds(values){
+    for(let i=0; i<values.length; i++){
+        if(i%2 == 0 ){
+            return i
+        }
+    }
+}
+console.log(noOdds([1,2,3,4,5]))
