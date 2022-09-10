705e8ddec6a1ad8ab66af497f30bb74319098426
Aysun <aysunkapusuz@gmail.com>
2022-09-10 12:36:31 -0600

    new solution

--- codeWars-7kyu/oddOrEven.js
+++ codeWars-7kyu/oddOrEven.js
@@ -15,3 +15,17 @@
 // const ODD = "Odd";
 // const EVEN = "Even";
 // const EITHER = "Either";
+
+function oddOrEven (n){
+    if (n !== 1){
+        if(n%2 === 1){
+            return 'Either'
+        }
+        if (n/2 %2 === 0){
+            return 'Even'
+        } else {
+            return 'Odd'
+        }
+    }
+    return 'Either'
+}
