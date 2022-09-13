a8b5476f65c0db4fbe5bc2ecb7f85e1e3c5ae22f
Aysun <aysunkapusuz@gmail.com>
2022-09-13 08:09:44 -0600

    another solution

--- codeWars-7kyu/sumOfOddNumbers.js
+++ codeWars-7kyu/sumOfOddNumbers.js
@@ -1,14 +1,19 @@
 // DESCRIPTION:
 // Given the triangle of consecutive odd numbers:
 // 
-//              1
-//           3     5
-//        7     9    11
-//    13    15    17    19
-// 21    23    25    27    29
+//              1                 =1
+//           3     5              =8     2^3
+//        7     9    11           =27    3^3
+//    13    15    17    19        =64    4^3
+// 21    23    25    27    29     =125   5^3
 // ...
 // Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 // 
 // 1 -->  1
 // 2 --> 3 + 5 = 8
 
+
+function rowSumOddNumbers(n){
+    return Math.pow(n,3)
+}
+console.log(rowSumOddNumbers(3))
