96892eda662c2f2f2c6835e84648da785b9728a1
Aysun <aysunkapusuz@gmail.com>
2022-09-15 07:35:54 -0600

    another solution

--- countSheep.js
+++ countSheep.js
@@ -4,7 +4,13 @@
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
 
- const countSheep = sheepNum => [...Array(sheepNum)].map((_, i) => `${i + 1} sheep...`).join('');
+function countSheep (num){
+    let str = "";
+    for(let i=1; i<=num; i++){
+        str += `${i} sheep...`
+    }
+    return str
+  }
 
 
 console.log(countSheep(12))
