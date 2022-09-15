e4505c18ea908568812096b41c7eed37419f4bf1
Aysun <aysunkapusuz@gmail.com>
2022-09-15 08:14:16 -0600

    another solution

--- codeWars-7kyu/testing123.js
+++ codeWars-7kyu/testing123.js
@@ -8,3 +8,13 @@
 // 
 // [] --> []
 // ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+//PARAMETERS: an array of varying length containing string values
+// RESULT: an array of strings contaning the original values with theline number and a colon + space in front 
+// EXAMPLES: ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+//PSEUDOCODE: iterate over each array value and prepend the line number starting with one and add a colon and aspace between the line number and the value
+
+let number = function (array){
+    console.log(array.map((el, index) => `${index + 1}: ${el}`))
+}
+number(["a","b","c"])
