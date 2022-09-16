6781a390e58f8db06a0eeb26bc852408a81c58ee
Aysun <aysunkapusuz@gmail.com>
2022-09-16 08:10:02 -0600

    another solution

--- grasshopper.js
+++ grasshopper.js
@@ -4,3 +4,7 @@
 // checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
 // 
 // The function receives one parameter health which will always be a whole number between -10 and 10.
+
+function checkAlive (health) {
+    return health>0 ? true: false
+  }
