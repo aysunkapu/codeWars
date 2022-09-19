e7b06950d9f87f8b62cbab544b1d7094a7b692de
Aysun <aysunkapusuz@gmail.com>
2022-09-19 07:31:01 -0600

    another solution

--- gravity.js
+++ gravity.js
@@ -36,3 +36,16 @@
 // return value must be Newton for force (obviously)
 // 
 // μ copy this from here to use it in your solution
+
+function solution([m1,m2,d],[um1,um2,ud]) {
+
+    const G = 6.67e-11 ;
+    
+    const conversion = { kg:1, g:1e-3, mg:1e-6, μg:1e-9, lb:.453592
+    
+    , m:1, cm:1e-2, mm:1e-3, μm:1e-6, ft:.3048
+    
+    } ;
+    
+    return G * m1 * conversion[um1] * m2 * conversion[um2] / ( d * conversion[ud] ) ** 2 ;
+}
