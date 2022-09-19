c8f67d8fc5dc4639df3d0483faf2fb63d9174806
Aysun <aysunkapusuz@gmail.com>
2022-09-19 07:55:27 -0600

    another solution

--- findNearetsSquareNumber.js
+++ findNearetsSquareNumber.js
@@ -2,3 +2,8 @@
 // Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
 // 
 // Goodluck :)
+
+function nearestSquareNumber (n){
+    return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+console.log(nearestSquareNumber(12))
