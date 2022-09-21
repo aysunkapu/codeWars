ecc51a77232208ecd2a73907dfb7af8cb67951f3
Aysun <aysunkapusuz@gmail.com>
2022-09-21 09:02:31 -0600

    another solution

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5 
+
+for (let i = 1; i < 100; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
+console.log(i)
