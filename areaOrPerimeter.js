8f58672e458cba632b6575799bb20d3272fb822d
Aysun <aysunkapusuz@gmail.com>
2022-10-03 11:10:28 -0600

    the problem resolved with PREP

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -8,6 +8,12 @@
 // 3, 3 --> 9
 // Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 
-const areaOrPerimeter = function(l , w) {
-    return l==w ? l*w : (l+w)*2
+const areaOrPerimeter = function(length , width) {
+    return length==width ? length*width : (length+width)*2
   };
+ console.log(areaOrPerimeter(10,5))
+//PREP
+//Parameters: are numbers with length and width
+//Return : is a number
+//Example : 6, 10 --> 32 3, 3 --> 9
+//Pseudocode : write a function that takes numbers and if it is a square return its area, if its rec return its perimeter
