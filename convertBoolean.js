c70d6150d7a8b920d37921c2856dcef7f4813be3
Aysun <aysunkapusuz@gmail.com>
2022-10-03 11:44:57 -0600

    the problem resolved with PREP

---
+++ convertBoolean.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+function converBoolean (boolean){
+    return boolean ? "Yes" : "No"
+}
+//PREP
+//Parameter : is a boolean
+//Return : is a string
+// Example : true => "Yes" false => "No"
+//Pseudocode : write a function which takes boolean and return "Yes" if the parameter is true, otherwise return "No"
