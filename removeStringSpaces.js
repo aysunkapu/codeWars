1dac4cb04b2eca43fd3775e702be4509d8ca896e
Aysun <aysunkapusuz@gmail.com>
2022-10-03 11:52:59 -0600

    the problem resolved with PREP

--- removeStringSpaces.js
+++ removeStringSpaces.js
@@ -1,5 +1,11 @@
 // Simple, remove the spaces from the string, then return the resultant string.
 
-const noSpace = (string) => string.replace(/\s*/g, '')
+const noSpace = (string) => string.split(' ').join('')
 
 console.log(noSpace("Hello World"))
+
+//PREP
+//Parameter : is a string
+//Return : is a string as well
+//Example : Hello World => HelloWorld
+//Pseudocode : write a function that takes a string and return the string with no space
