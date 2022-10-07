6cfe685b5ec9492a305771e4fb5b2f248ceb46c6
Aysun <aysunkapusuz@gmail.com>
2022-10-07 15:44:22 -0600

    problem resolved with PREP

---
+++ returningstring.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+// 
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+function greeting (name){
+    return `Hello, ${name} how are you doing today?`
+}
+console.log(greeting('Aysun'))
+//PREP
+//Parameter : is a string. 
+//Return : is "Hello, <name> how are you doing today?"
+//Example : Aysun => "Hello, Aysun how are you doing today?"
+//Pseudocode : write a function and return "Hello, <name> how are you doing today?"
