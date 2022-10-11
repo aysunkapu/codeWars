3e09dd2654c5b271476708f748e06bef7b5a1e96
Aysun <aysunkapusuz@gmail.com>
2022-10-11 12:55:28 -0600

    another solution

--- sentenceSmash.js
+++ sentenceSmash.js
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
 // Sentence Smash
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 // 
 // Example
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
-
-
-function sentSmash (sent){
-    return sent.join(" ")
-}
-
-=======
-// Sentence Smash
-// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 // 
-// Example
-// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
-
-
-function sentSmash (sent){
-    return sent.join(" ")
-}
-
->>>>>>> 57f16c90aeb962d059efa6a115b407fc096e5a60
-console.log(sentSmash(["Have", "a", "good", "day!"]))
+function smash (words) {
+    return words.join(" ")
+ };
