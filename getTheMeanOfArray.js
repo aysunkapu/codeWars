3173f22f95174d22a40918770a01a9227ae841b6
Aysun <aysunkapusuz@gmail.com>
2022-10-14 11:04:46 -0600

    another solution

---
+++ getTheMeanOfArray.js
@@ -0,0 +1,20 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// 
+// Return the average of the given array rounded down to its nearest integer.
+// 
+// The array will never be empty.
+
+function getAverage(marks){
+    let result = 0;
+    
+    for (let el of marks)
+      result += el;
+    
+    return Math.floor(result/marks.length)
+  }
+  
+  //prep
+  //parameter is an array
+  // return is an average of the array
+  // example ([2,2,2,2]),2);
+  //         ([1,2,3,4,5,]),3);
