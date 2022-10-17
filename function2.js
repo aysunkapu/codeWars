1960f1ac3d98a9fd39e2adf5bdf6acd0ca01e363
Aysun <aysunkapusuz@gmail.com>
2022-10-17 07:27:27 -0600

    another solution

---
+++ function2.js
@@ -0,0 +1,13 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+function square (n){
+    return n*n
+  }
+  
+  
+  //prep
+  //parameter is a number
+  //return is square of the num
+  //example square(3), 9
