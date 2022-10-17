95ebbe474a0c0927c806f4b82676c07267ce5029
Aysun <aysunkapusuz@gmail.com>
2022-10-17 07:44:31 -0600

    another solution

---
+++ countOddNumbersBelown.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Given a number n, return the number of positive odd numbers below n, EASY!
+// 
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
+function oddCount(n){
+    return parseInt(n/2);
+   }
+function odd (num){
+    return Math.floor(n/2)
+}   
+
+//prep
+//paramter is a single num
+//return is an odd num of until the num
+// example 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
