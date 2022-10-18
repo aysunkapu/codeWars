bb4e1cb1d32a51f9efa12e9539dd92b4d4f85573
Aysun <aysunkapusuz@gmail.com>
2022-10-18 09:07:17 -0600

    resolution with prep

--- catAndDog.js
+++ catAndDog.js
@@ -20,8 +20,12 @@
 let humYearsCatYearsDogYears = function(humanYears){
     let catYears = humanYears < 2 ? 15 : humanYears <3 ? 24 : (humanYears - 2)*4 + 24
     let dogYears = humanYears <2 ? 15 : humanYears < 3 ? 24 : (humanYears - 2)*5 + 24
-    return [humanYears,catYears,dogYears]
-    
+    return [humanYears,catYears,dogYears]   
 }
+
+//prep 
+//parameter is a number (humanYears)
+//returns are 3 numbs (humanYears,catYears,dogYears)
+//example (10), [10,56,64]
 
 
