31c001ffe15f7e9282fb528ea9d8ded71bd864c4
Aysun <aysunkapusuz@gmail.com>
2022-10-26 15:10:42 -0600

    another solution

---
+++ binToDecimal.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    return parseInt(bin, 2)
+  }
+
+  //prep
+  //parameter is a binary number
+  //return the binary num to decimal num
+  //example ["1",1], ["0",0], ["1001001", 73] ]
