ac36fa9b185d7be57150b30ff3a75f59624b7e91
Aysun <aysunkapusuz@gmail.com>
2022-11-23 09:04:17 -0700

    another solution

---
+++ standup/dupe.js
@@ -0,0 +1,13 @@
+// The goal of this exercise is to convert a string to new string where is character in the new string is "(" if that character appear only once in the original string, or ")" if that character appears more more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+//examples
+//din => (((
+// recede => ()()()
+// Success => )())())
+// (( #   => ))((
+
+
+function encodeDupes (string){
+    return string.toLowerCase().split('').map( (l,i,a) => a.indexOf(l) === a.lastIndexOf(l) ? '(' : ')').join('')
+}
+
+console.log(encodeDupes('din'), '(((')
