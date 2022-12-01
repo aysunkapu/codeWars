17659e45a29d77c550add9896e7b538d78309b74
Aysun <aysunkapusuz@gmail.com>
2022-12-01 08:37:39 -0700

    another solution

---
+++ standup/removeDuppWords.js
@@ -0,0 +1,10 @@
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. Fox example:
+// "alpha beta beta gamma gamma gamma  delta alpha beta beta gamma gamma gamma delta"
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+function removeDupeWords(str){
+    // str -> split " "-> filter
+    return str.split(" ").filter((w,i,a)=> w != a[i-1]).join(" ")
+}
+
+console.log(removeDupeWords("alpha beta beta gamma gamma gamma  delta alpha beta beta gamma gamma gamma delta"), "alpha beta gamma delta alpha beta gamma delta" )
