095dd78bc731ba973ad8d376b34cc24229c7d604
Aysun <aysunkapusuz@gmail.com>
2022-12-07 09:26:39 -0700

    another solution

---
+++ standup/strArrDupes.js
@@ -0,0 +1,17 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the arrray.
+
+// For Example:
+// dup(["abracadabra", "allottee", "assessee"]) = ["abracadabra", "alote", "asese"]
+// dup (["kelless", "keenness"]) = ["keles", "kenes"]
+
+
+//String will be lowercase only, no spaces
+
+function stringDup(arr){
+// map-> word-split-filter-join
+  return arr.map(
+    word => word.split('').filter((l,i,a)=> l !=a[i-1]).join('')
+  )
+}
+
+console.log(stringDup(["kelless", "keenness"]), "['keles', 'kenes']")
