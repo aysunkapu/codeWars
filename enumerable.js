cac653f4981bd3dda011e9f81febb06e62328a1c
Aysun <aysunkapusuz@gmail.com>
2022-12-21 07:16:25 -0700

    another solution

---
+++ enumerable.js
@@ -0,0 +1,23 @@
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+
+
+function none(arr, fun){
+    for(let i=0; i<arr.length; i++){
+        if(fun(arr[i]) == true){
+            return false;
+        }
+    }
+    return true;
+  }
+
+  function none(arr, fun){
+    return !arr.some(fun);
+  }
+  
+  
+
+ // none([1,2,3,4,5],function(item){ return item > 5 }), true
+// none([1,2,3,4,5],function(item){ return item > 4 }), false
+
+
+
