85bf2bcf08becafe1a8766808d36b3681be88282
Aysun <aysunkapusuz@gmail.com>
2022-12-26 08:45:57 -0700

    another solution

---
+++ pillars.js
@@ -0,0 +1,21 @@
+//There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+//
+//number of pillars (≥ 1);
+//distance between pillars (10 - 30 meters);
+//width of the pillar (10 - 50 centimeters).
+//Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+
+function pillars (numPil, dist, width){
+    if(numPil <2 ){
+        return 0
+    }else if(numPil < 3){
+        return dist*100
+    }else{
+        return ((numPil-2)*width) + ((100*dist)*(numPil-1))
+    }
+    
+}
+
+console.log(pillars(2, 20, 25))
+console.log(pillars(11, 15, 30))
