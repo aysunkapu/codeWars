4436fa8b1452e563e74a299e00ba73f9d0ea786e
Aysun <aysunkapusuz@gmail.com>
2023-01-14 07:05:34 -0700

    another solution

--- codeWars-8kyu/basicMathematicalOperation.js
+++ codeWars-8kyu/basicMathematicalOperation.js
@@ -9,15 +9,31 @@
 // ('*', 5, 5) --> 25
 // ('/', 49, 7) --> 7
 
-function basicOp(operation, value1, value2)
-{
-  if(operation === '+'){
-    return value1 + value2
-  }else if(operation === '-'){
-    return value1 - value2
-  }else if(operation === '*'){
-    return value1 * value2
-  }else{
-    return value1 / value2
-  }
+// function basicOp(operation, value1, value2)
+// {
+//   if(operation === '+'){
+//     return value1 + value2
+//   }else if(operation === '-'){
+//     return value1 - value2
+//   }else if(operation === '*'){
+//     return value1 * value2
+//   }else{
+//     return value1 / value2
+//   }
+// }
+// 
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
 }
+console.log(basicOp('-',12,2))
