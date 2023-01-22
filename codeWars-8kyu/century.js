5ffceeb463acccdc67a76e6a0f880ba143c84958
Aysun <aysunkapusuz@gmail.com>
2023-01-22 07:21:25 -0700

    another solution

--- codeWars-8kyu/century.js
+++ codeWars-8kyu/century.js
@@ -11,6 +11,16 @@
 function centuryCall (year){
     return Math.ceil(year/100)
 }
+//another solution
+function centuryCall(year){
+    let century = 0;
+    for(let i=0; i<year; i++){
+        if(i%100 == 0){
+            century++
+        }
+    }
+    return century
+}
 
 console.log(centuryCall(2005))
 
