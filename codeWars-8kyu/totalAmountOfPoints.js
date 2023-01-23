63e6e98c41c56fa3c6da340034b5a984c222e3ed
Aysun <aysunkapusuz@gmail.com>
2023-01-23 07:38:06 -0700

    another solution

--- codeWars-8kyu/totalAmountOfPoints.js
+++ codeWars-8kyu/totalAmountOfPoints.js
@@ -28,6 +28,25 @@ function points(games){
     }, 0);
 }
 
+function points (gamesArr){
+    let totalPoints = 0;
+
+    for(let i=0; i<gamesArr.length; i++){
+        const value = gamesArr[i]
+
+        const parts = value.split(":")
+        const x =parts[0];
+        const y = parts[1];
+
+        if(x>y){
+          totalPoints+=3
+        }else if(x===y){
+            totalPoints+=1
+        }
+    }
+    return totalPoints
+}
+
 console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30)
 console.log(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10)
 console.log(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0)
