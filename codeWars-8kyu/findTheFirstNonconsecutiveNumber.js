282636c6825fe24a122c37e4cbabeefe1b741cd9
Aysun <aysunkapusuz@gmail.com>
2023-01-25 11:08:58 -0700

    another solution

--- codeWars-8kyu/findTheFirstNonconsecutiveNumber.js
+++ codeWars-8kyu/findTheFirstNonconsecutiveNumber.js
@@ -20,6 +20,16 @@ function firstNonCons(arr){
     }
     return null
 }
+//simpler code
+
+function firstNonCons(arr){
+    for(let i=0; i<arr.length-1; i++){
+        if(arr[i] + 1 !== arr[i+1]){
+           return arr[i+1]
+        }
+    }
+    return null
+}
 
 
 console.log(firstNonCons([1,2,3,5]))
