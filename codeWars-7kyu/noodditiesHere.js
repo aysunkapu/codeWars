5345121a7af1c8b1988433da81d02ded1ad153b8
Aysun <aysunkapusuz@gmail.com>
2023-01-26 10:45:04 -0700

    another solution

--- codeWars-7kyu/noodditiesHere.js
+++ codeWars-7kyu/noodditiesHere.js
@@ -2,6 +2,11 @@
 // all values in the arrar will be integers. return the goood values in the order they are given.
 // function noOdds(values)
 
+
+// parameter is an array with numbers
+//eliminate odd numbers and return even numbs
+//example [1,2,3,4,5,6,7,8] => [2,4,6,8]
+
 function noOdds(values){
     return values.filter(el => el%2 === 0);
 }
