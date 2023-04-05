df7e6d7b623fd43758e6e5803a139e7fbd88f40b
aysun <aysunkapusuz@gmail.com>
2023-04-05 11:26:54 -0600

    another solution

---
+++ codeWars-8kyu/maxHeadroom.js
@@ -0,0 +1,35 @@
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+
+// function getMax1()
+// {
+//   var max = 
+//   {
+//    name: 'Max Headroom'
+//   }
+//   return max;
+// }
+// 
+// function getMax2()
+// {
+//   return
+//   {
+//     name: 'Max Headroom'
+//   }
+// }
+
+
+// debugging
+
+function getMax1()
+{
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2()
+{
+// Have your return object on same line
+return {name: 'Max Headroom'};
+}
+
