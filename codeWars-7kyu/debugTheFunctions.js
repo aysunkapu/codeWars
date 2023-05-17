3cb01eb774684aeed23fc3e69a14d129e923429e
aysun <aysunkapusuz@gmail.com>
2023-05-17 07:40:23 -0400

    another solution

---
+++ codeWars-7kyu/debugTheFunctions.js
@@ -0,0 +1,26 @@
+// Should be easy, begin by looking at the code. Debug the code and the functions should work.
+// 
+// There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+
+
+
+function multi(arr) {
+    return arr * arr;
+  }
+  function add(arr) {
+    return arr + arr;
+  }
+  function reverse(str) {
+    return str.reverse();
+  }
+  
+  
+  function multi(arr) {
+    return arr.reduce((a,b)=> a*b, 1);
+  }
+  function add(arr) {
+    return arr.reduce((a,b)=> a+b, 0);
+  }
+  function reverse(str) {
+    return str.split('').reverse().join('');
+  }
