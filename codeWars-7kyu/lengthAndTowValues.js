ce89192f497d47e3c2207f0d961147102ddd49e6
aysun <aysunkapusuz@gmail.com>
2023-06-13 07:39:03 -0400

    another solution

---
+++ codeWars-7kyu/lengthAndTowValues.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Given an integer n and two other values, build an array of size n filled with these two values alternating.
+// 
+// Examples
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+
+function alternate (n, firstValue, secondValue){
+    const arr = [];
+    for(let i=0; i<n; i++){
+        arr.push(i%2===0? firstValue : secondValue)
+    }
+    return arr;
+}
